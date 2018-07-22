22da81037f55b0f0eb8146edda8f87845e03214d
Gui Yazbek <>
2018-07-22 11:51:44 -0700

    Added gulp to manage scss and browsersync

---
+++ gulpfile.js
@@ -0,0 +1,129 @@
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    rename      = require('gulp-rename'),
+    cleanCSS    = require('gulp-clean-css'),
+    concat      = require('gulp-concat'),
+    uglify      = require('gulp-uglify'),
+    prefix      = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync'),
+    reload      = browserSync.reload,
+    htmlmin     = require('gulp-htmlmin'),
+    size        = require('gulp-size'),
+    sassLint    = require('gulp-sass-lint'),
+    del         = require('del'),
+    vinylPaths  = require('vinyl-paths'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    runSequence = require('run-sequence'),
+    deploy      = require('gulp-gh-pages');
+
+
+var bases = {
+    app:  'src/',
+    dist: 'dist/',
+};
+
+
+var sassOptions = {
+  outputStyle: 'expanded'
+};
+
+
+gulp.task('clean:dist', function() {
+  return gulp.src(bases.dist)
+    .pipe(vinylPaths(del));
+});
+
+
+gulp.task('styles', function() {
+  return gulp.src(bases.app + 'scss/styles.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions))
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(prefix())
+    .pipe(rename('styles.css'))
+    .pipe(gulp.dest(bases.dist + 'css'))
+    .pipe(reload({stream:true}))
+    .pipe(cleanCSS({debug: true}, function(details) {
+      console.log(details.name + ': ' + details.stats.originalSize);
+      console.log(details.name + ': ' + details.stats.minifiedSize);
+    }))
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(bases.dist + 'css'))
+});
+
+gulp.task('themes', function() {
+  return gulp.src(bases.app + 'scss/themes/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions))
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(prefix())
+    .pipe(gulp.dest(bases.dist + 'css/themes'))
+    .pipe(reload({stream:true}))
+    .pipe(cleanCSS({debug: true}, function(details) {
+      console.log(details.name + ': ' + details.stats.originalSize);
+      console.log(details.name + ': ' + details.stats.minifiedSize);
+    }))
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(bases.dist + 'css/themes'))
+});
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: bases.dist
+    }
+  });
+});
+
+gulp.task('js-app', function() {
+  gulp.src(bases.app + 'js/*.js')
+    .pipe(uglify())
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest(bases.dist + 'js'))
+    .pipe(reload({stream:true}));
+});
+
+gulp.task('js-libs', function() {
+  gulp.src([bases.app + 'js/libs/*.js', '!' + bases.app + 'js/libs/modernizr.js'])
+    .pipe(uglify())
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(concat('libs.js'))
+    .pipe(gulp.dest(bases.dist + 'js'))
+    .pipe(reload({stream:true}));
+});
+
+
+gulp.task('sass-lint', function() {
+  gulp.src([bases.app + 'scss/**/*.scss', '!' + bases.app + 'scss/libs/**/*.scss', '!' + bases.app + 'scss/states/_print.scss'])
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError());
+});
+
+gulp.task('minify-html', function() {
+  gulp.src(bases.app + './*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest(bases.dist))
+    .pipe(reload({stream:true}));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(bases.app + 'scss/**/*.scss', ['styles']);
+  gulp.watch(bases.app + './*.html', ['minify-html']);
+});
+
+gulp.task('default', function(done) {
+  runSequence('clean:dist', 'browser-sync', 'js-app', 'js-libs', 'minify-html', 'styles', 'themes', 'watch', done);
+});
+
+gulp.task('build', function(done) {
+  runSequence('clean:dist', 'js-app', 'js-libs', 'minify-html', 'styles', done);
+});
+
+gulp.task('deploy', function() {
+    return gulp.src(bases.dist)
+      .pipe(deploy());
+  });
